4917e2baa9aaf8e8656adec85039ccccf27f2e34
unknown <vldbrtkv95@gmail.com>
2023-03-17 15:01:37 +0300

    Домашнее Задание 4

---
+++ src/components/SeachInfo/index.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+
+const SeachInfo = ({searchText, searchCount}) => {
+	return (
+		searchText && <section className="search-title">
+			По запросу <span>{searchText}</span> найдено 
+			{searchCount} товаров
+		</section>
+	);
+};
+
+export default SeachInfo;
